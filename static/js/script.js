a8e59d76d1b573d36f2dbe5b17c24e81d890f3c7
Augusto Corvalan <augustocorvalan@gmail.com>
2014-03-01 17:45:10 -0500

    ajaxing in updated temperature

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,10 @@
 $(function () {
-	$.setTimeout(function () {
-
+	var $temp = $(".temp"),
+		$body = $("body");
+	setInterval(function () {
+		$.getJSON($SCRIPT_ROOT + '/update', function (data) {
+     	   $temp.text(data.temp);
+     	   $body.removeClass().addClass(data.klass);
+	    });
 	}, 1000);
 });
